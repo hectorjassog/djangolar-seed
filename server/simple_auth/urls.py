b111afaaab277f6fa9e40cc7be515cb7e5a591b6
Francky <francky.blondy@hxperience.com>
2016-05-04 16:17:13 +0200

    add a custom form to auth, for placeholders

--- server/simple_auth/urls.py
+++ server/simple_auth/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import url
 import django.contrib.auth.views as auth_views
 from django.core.urlresolvers import reverse
 
-
+from .forms import SimpleAuthForm
 
 urlpatterns = [
     url(r'^in/$',
         auth_views.login,
-        {'template_name': 'simple_auth/login.html'},
+        {'template_name': 'simple_auth/login.html',
+         'authentication_form': SimpleAuthForm },
         name='simple_login'),
     url(r'^out/$',
         auth_views.logout_then_login,
